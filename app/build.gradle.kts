dfe8f5e335cf6538e3d1f9936ae39355550106b0
Ramesh R <rameshgascngl@gmail.com>
2026-09-24 18:28:23 +0530

    Add SQLCipher release dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -121,6 +121,8 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.sqlite:sqlite:2.7.0")
+    implementation("net.zetetic:sqlcipher-android:4.19.0")
 
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
